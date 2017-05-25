77dc641dff9614ae1444c52d02194aa7e1894db7
John Schmid <jschmid808@gmail.com>
2017-05-25 18:05:30 -0400

    Final

--- Angular/partials/client/main.js
+++ Angular/partials/client/main.js
@@ -1,6 +1,5 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
-//Routes
 app.config(function($routeProvider){
 	$routeProvider
 		.when("/users", {
@@ -13,3 +12,56 @@ app.config(function($routeProvider){
 			redirectTo: "/users"
 		});
 });
+
+app.factory("userFactory", [function(){
+	var factory = {};
+	var users = [
+		{firstName: "Stunna", lastName: "Greatest", lang: "Java"},
+		{firstName: "Billy", lastName: "Madison", lang: "Ruby"},
+	];
+
+	//Passes the user list to controller
+	factory.index = function(callback){
+		callback(users);
+	}
+
+	factory.create = function(user){
+		users.push(user);
+	}
+
+	factory.delete = function($index){
+		users.splice($index, 1);
+	}
+	return factory;
+}]);
+
+app.controller("customController", ['$scope','userFactory', function($scope, userFactory){
+	function setUsers(data){
+		$scope.users = data;
+		$scope.newUser = {};
+	}
+
+	$scope.users = [];
+
+	//Retrieves user list
+	userFactory.index(setUsers);
+
+	$scope.create = function(){
+		userFactory.create($scope.newUser);
+		$scope.newUser = {}; //Resets form
+	}
+
+	$scope.delete = function($index){
+		userFactory.delete($index);
+	}
+}]);
+
+app.controller("userListController", ['$scope','userFactory', function($scope, userFactory){
+	function setUsers(data){
+		$scope.users = data;
+	}
+
+	$scope.users = [];
+
+	userFactory.index(setUsers);
+}]);
